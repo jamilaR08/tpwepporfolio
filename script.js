7f51e49f11f3302fc62ea7165c66029af68af39b
jamilaR08 <r.maimounajamila@esisa.ac.ma>
2026-05-18 16:08:49 +0100

    ok

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-const nom = document.getElementById("nom");
-const email = document.getElementById("email");
+console.log("Hello, World!");
+// alert("Welcome to the site!");
+
+const name = document.getElementById("nom");
 const password = document.getElementById("password");
-const button = document.querySelector("btn");
+const email = document.getElementById("email");
+const btnSend = document.getElementById("btnSend");
 
 
-console.log(nom);
-console.log(email);
-console.log(password);
-console.log(button);
+btnSend.addEventListener("click", function() {
+
+    if (name.value === "" || password.value === "" || email.value === "") {
+        alert("Please fill in all fields.");
 
 
-button.addEventListener("click", function(event){
-    event.preventDefault();
-    console.log(nom.value);
-    console.log(email.value);
-    console.log(password.value);
-
-const user={
-    name: nom.value,
-    email: email.value,
-    password: password.value
-};
-localStorage.setItem('user',JSON.stringify(user));
-
-
-    alert("Name: " + nom.value + "\nEmail: " + email.value + "\nPassword: " + password.value);
+    }
+     else if (!email.value.includes("@")) {
+            alert("Please enter a valid email address.");
+        }
+         else {
+        alert(name.value + " " + password.value + " " + email.value);
+    }
 });
 
+
+const formInscription = document.getElementById("formSignup");
+
+const signupbutton = document.getElementById("signupbtn");
+
+signupbutton.addEventListener("click", function() {
+    formInscription.style.display = "flex";
+        formConnexion.style.display = "none";
+
+});
+
+const formConnexion = document.getElementById("formSignIn");
+
+const loginbutton = document.getElementById("signinbtn");
+
+loginbutton.addEventListener("click", function() {
+    formConnexion.style.display = "flex";
+        formInscription.style.display = "none";
+
+});
